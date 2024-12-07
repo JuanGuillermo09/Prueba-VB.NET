880fe814c471bcbb753edf476cdb482c2ccb6086
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-07 15:44:36 -0500

    Video 3 Declaración de variables y constantes

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -2,9 +2,31 @@
 
     Sub Main()
 
+        ' Menjase por CMD.
+
         Console.WriteLine("Hola mundo")
 
         Console.ReadKey(True)
+
+        ' Variables.
+
+        Dim numero As Integer = 12
+
+        Dim pulgada As Double = 2.54
+
+        Dim letra As Char = "a"
+
+        Dim cadena As String = "Esto es una cadena"
+
+        Dim condicion As Boolean = True ' O False.
+
+        ' Constante.
+
+        Const PULGADA As Double = 2.54
+
+
+
+
 
     End Sub
 
