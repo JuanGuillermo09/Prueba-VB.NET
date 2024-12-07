6d139f286f95848518f509ab7e8c12ff733bc6eb
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-07 18:23:21 -0500

    Video 6 Entrada de datos por consola

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -3,87 +3,17 @@
 
     Sub Main()
 
-        Dim aritmeticos As Double = 8 + 3
-        Dim aritmetico As Double = 8 - 3
-        Dim aritmetic As Double = 8 * 3
-        Dim aritmeti As Double = 8 / 3
-        Dim aritmet As Double = 8 Mod 3
+        Dim miNombre As String = "Pedro"
 
-        Dim aritme As Double = 8
+        Console.WriteLine(miNombre)
 
-        aritme += 2
+        Console.Write("Ingrese su nombre ..: ")
 
-        Dim aritm As Double = 8
+        Dim Nombre As String = Console.ReadLine()
 
-        aritm -= 2
+        Console.WriteLine(Nombre)
 
-        Dim arit As Double = 8
-
-        arit -= 2
-
-        arit += 8
-
-
-        Console.WriteLine(aritmeticos)
-
-        Console.WriteLine(aritmetico)
-
-        Console.WriteLine(aritmetic)
-
-        Console.WriteLine(aritmeti)
-
-        Console.WriteLine(aritmet)
-
-        Console.WriteLine(aritme)
-
-        Console.WriteLine(aritm)
-
-        Console.WriteLine(arit)
-
-
-        Dim condicion As Boolean = -14 > 2
-        Dim condicio As Boolean = -14 < 2
-        Dim condici As Boolean = 2 <= 2
-        Dim condic As Boolean = 2 >= 2
-        Dim condi As Boolean = (2 = 2)
-
-        Dim cond As Boolean = 7 > 4 And 3 > 6
-
-        Dim con As Boolean = 7 > 4 And 3 < 6
-
-        Dim co As Boolean = 7 > 4 Or 3 > 6
-
-        Dim c As Boolean = 7 < 4 Or 3 > 6
-
-        Dim c1 As Boolean = Not 14 > 8
-
-
-
-        Console.WriteLine(condicion)
-
-        Console.WriteLine(condicio)
-
-        Console.WriteLine(condici)
-
-        Console.WriteLine(condic)
-
-        Console.WriteLine(condi)
-
-        Console.WriteLine(cond)
-
-        Console.WriteLine(con)
-
-        Console.WriteLine(co)
-
-        Console.WriteLine(c)
-
-        Console.WriteLine(c1)
-
-
-
-
-
-
+        Console.WriteLine("Bienvenido al sistema sr(a) : " & Nombre)
 
 
 
