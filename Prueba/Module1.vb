6419f79c72f0806f677b318e6150dd051cf5ce54
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-07 19:05:08 -0500

    Video 8 Sentencia If ElseIf

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -3,32 +3,36 @@
 
     Sub Main()
 
-        'Realizar un programa que muestre por consola si un estudiante con el dinero que tiene ahorrado '
-        'puede pagar la universidad y estudiar, Teniendo en cuenta que el semestre del programa'
-        'que estudia tiene un valor de 600'
+        'Elaborar un programa de consola en Visual Basic. Net, que ingresado el valor total por teclado de una compra '
+        'realice un descuento de acuerdo a los siguiente: si el valor comprado esta entre 50-100 el descuento es del 5%,'
+        'si esta entre 101-150 el descuento es del 10% y si la compra es mayor a 150 el descuento es del 15%,'
+        'el programa debe mostrar el total de la compra, el total de la compra con el descuento y el descuento'.
 
-        Dim estudia As Boolean = False
 
-        Console.Write("Ingrese el dinero ahorrado---:")
-        Dim dineroAhorrado As Double = Console.ReadLine()
 
-        If dineroAhorrado >= 600 Then
+        Console.Write("Ingrese el valor total de la compra---:")
+        Dim valorCompra As Double = Console.ReadLine()
 
-            estudia = True
+        Dim descuento As Double = 0
 
+
+        If valorCompra >= 50 And valorCompra <= 100 Then
+
+            descuento = valorCompra * 0.05
+
+        ElseIf valorCompra > 100 And valorCompra <= 150 Then
+
+            descuento = valorCompra * 0.1
+
+        ElseIf valorCompra > 150 Then
+
+            descuento = valorCompra * 0.15
 
         End If
 
-        If estudia = True Then
-
-            Console.WriteLine("El dinero ahorrado sirve para pagar la universidad, por lo tanto puedes estudiar")
-
-        Else
-
-            Console.WriteLine("El dinero ahorrado no sirve para pagar la universidad, por lo tanto no puedes estudiar")
-
-
-        End If
+        Console.WriteLine("El valor total de la compra sin descuento es ---: " & valorCompra)
+        Console.WriteLine("El valor total de la compra con descuento es ---: " & valorCompra - descuento)
+        Console.WriteLine("El valor descontado en la compra es ---: " & descuento)
 
     End Sub
 
