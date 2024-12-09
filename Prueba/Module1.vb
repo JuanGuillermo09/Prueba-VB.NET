46612d4ccd31ad540ce7c92ef2aaa41fb9b37aa7
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-09 10:38:24 -0500

    Video 10 Bucle For

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -1,63 +1,35 @@
 ﻿Module Module1
 
-
     Sub Main()
 
-        'Elaborar un programa de consola en Visual Basic. Net, que permita realizar una operacion basica matematica '
-        'entre dos numeros, teniendo en cuenta la opcion del usuario por teclado, siendo 1.Suma, 2.Resta,'
-        '3.Multiplicacion, 4.Divicion y 5.Porcentaje.'
+        For i = 1 To 3 Step 1
 
+            Console.WriteLine("Este es el mejor curso de vb. net")
 
-        Console.Write("Ingrese 1.Suma, 2.Resta, 3.Multiplicacion, 4.Divicion y 5.Porcentaje---:")
-        Dim opcion As Integer = Console.ReadLine()
+        Next
 
-        Dim numero1, numero2 As Double
+        'Hacer un algoritmo que imprima los primeros 20 terminos de la siguiente serie '
+        '1, 3, 6, 10, 15, 21, 28, ....... . .'
+        ''
 
-        Select Case opcion
+        Dim suma As Integer = 1
 
-            Case 1 : Console.Write("Ingrese el primer numero a sumar---:")
-                numero1 = Console.ReadLine()
-                Console.Write("Ingrese el segundo numero a sumar---:")
-                numero2 = Console.ReadLine()
+        For i = 2 To 20 Step 1
 
-                Console.WriteLine("La suma de los numeros es---:" & (numero1 + numero2))
+            If i <> 20 Then
 
-            Case 2 : Console.Write("Ingrese el primer numero a restar---:")
-                numero1 = Console.ReadLine()
-                Console.Write("Ingrese el segundo numero a restar---:")
-                numero2 = Console.ReadLine()
+                Console.Write(suma & " , ")
 
-                Console.WriteLine("La resta de los numeros es---:" & (numero1 - numero2))
+            Else
 
-            Case 3 : Console.Write("Ingrese el multiplicando---:")
-                numero1 = Console.ReadLine()
-                Console.Write("Ingrese el multiplicador---:")
-                numero2 = Console.ReadLine()
+                Console.WriteLine(suma)
 
-                Console.WriteLine("La multiplicacion de los numeros es---:" & (numero1 * numero2))
+            End If
 
-            Case 4 : Console.Write("Ingrese el dividendo---:")
-                numero1 = Console.ReadLine()
-                Console.Write("Ingrese el divisor---:")
-                numero2 = Console.ReadLine()
+            suma = suma + i
 
-                Console.WriteLine("La division de los numeros es---:" & (numero1 / numero2))
-
-            Case 5 : Console.Write("Ingrese el numero a sacar el porcentaje---:")
-                numero1 = Console.ReadLine()
-                Console.Write("Ingrese el porcentaje---:")
-                numero2 = Console.ReadLine()
-
-                Console.WriteLine("El " & numero2 & "% de " & numero1 & " es---:" & (numero1 * numero2) / 100)
-
-
-            Case Else
-                Console.WriteLine("La opcion ingresada no es valida")
-
-        End Select
-
+        Next
 
     End Sub
-
 
 End Module
