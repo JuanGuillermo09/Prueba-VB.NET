cceea8691ecf0b3b05723a53eab0fb8557228942
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-09 16:57:48 -0500

    Video 13 Bucles anidados

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -2,34 +2,17 @@
 
     Sub Main()
 
+        For i = 1 To 2 Step 1
 
+            Console.WriteLine("Este es el mensaje del primer bucle")
 
-        'si es false el bucle se repite'
+            For j = 1 To 3 Step 1
 
-        Dim numero As Integer
+                Console.WriteLine("Este es el mensaje del segundo bucle")
 
-        Do
-            Console.Write("Ingrese un numero-->")
-            numero = Console.ReadLine()
+            Next
 
-            Console.WriteLine("Este es el mejor curso de Visual Basec. Net")
-
-        Loop Until numero <> 0
-
-
-
-        'si es true el bucle se repite'
-
-        Dim numer As Integer
-
-        Do
-            Console.Write("Ingrese un numero-->")
-            numer = Console.ReadLine()
-
-            Console.WriteLine("Este es el mejor curso de Visual Basec. Net")
-
-        Loop While numer <> 0
-
+        Next
 
     End Sub
 
