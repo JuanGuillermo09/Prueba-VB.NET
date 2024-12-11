d94657e6f63fc5f1fbfa7c76d8f0cfdc8606a08a
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-11 13:13:49 -0500

    Video 14 Array o vectores y Bucle For Each

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -2,17 +2,59 @@
 
     Sub Main()
 
-        For i = 1 To 2 Step 1
+        'Valores del array nombres de la presentacion: Maria, Pedro, Daniela, Juan, Carlos, Camila '
 
-            Console.WriteLine("Este es el mensaje del primer bucle")
 
-            For j = 1 To 3 Step 1
 
-                Console.WriteLine("Este es el mensaje del segundo bucle")
+        ' 1mra forma de almacenar datos en una array'
 
-            Next
+        Dim nombres = New String() {"Maria", "Pedro", "Daniela", "Juan", "Carlos", "Camila"}
+
+        ' 2mra forma de almacenar datos en una array'
+
+        Dim nombre(6) As String
+
+        For i = 0 To nombre.Length - 1 Step 1
+
+            Console.Write("Ingrese el nombre en la posicion " & i & " -----)")
+
+            nombre(i) = Console.ReadLine()
 
         Next
+
+        ' 3mra forma de almacenar datos en una array'
+
+        Dim nombr(6) As String
+
+        nombr(0) = "Maria"
+        nombr(1) = "Pedro"
+        nombr(2) = "Daniela"
+        nombr(3) = "Juan"
+        nombr(4) = "Carlos"
+        nombr(5) = "Camila"
+
+        Console.WriteLine(nombr(3))
+
+        Dim nomb As String = nombr(4)
+
+        Console.WriteLine(nombr)
+
+        For i = 0 To nombr.Length - 1 Step 1
+
+            Console.WriteLine(nombr(i))
+
+        Next
+
+        For Each nom As String In nombr
+
+            Console.WriteLine(nom)
+
+        Next
+
+
+
+
+
 
     End Sub
 
