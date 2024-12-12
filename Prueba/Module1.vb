377a568f83e1abe7bad070df230d4798f92ce219
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-12 12:50:38 -0500

    Video 15-b  Anexo Arrays, Matrices y B.For Each

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -2,71 +2,29 @@
 
     Sub Main()
 
-        'Valores de la matrioz de la presentacion: 25, 13, 84, 6, 22, 39 '
+        Dim vector(2) As Integer
+        Dim matriz(1, 1) As Integer
 
-        '1mera Forma para almacenar datos a la matriz'
+        vector(0) = 15
+        vector(1) = 30
+        vector(2) = 45
 
-        Dim numeros = New Integer(,) {{25, 13, 84}, {6, 22, 39}}
+        matriz(0, 0) = 20
+        matriz(0, 1) = 40
+        matriz(1, 0) = 60
+        matriz(1, 1) = 80
 
+        For Each num As Integer In vector
 
-
-        '2mera Forma para almacenar datos a la matriz'
-
-        Dim numero(2, 3) As Integer
-
-        For filas = 0 To 1 Step 1
-
-            For columna = 0 To 2 Step 1
-
-                Console.Write("Ingrese el numero en la posicion " & filas & " , " & columna & "-----)")
-                numero(filas, columna) = Console.ReadLine()
-
-            Next
+            Console.Write(num & " ")
 
         Next
 
+        Console.WriteLine("")
 
+        For Each num As Integer In matriz
 
-        '3mera Forma para almacenar datos a la matriz'
-
-        Dim numer(2, 3) As Integer
-
-        numer(0, 0) = 25
-        numer(0, 1) = 13
-        numer(0, 2) = 84
-        numer(1, 0) = 6
-        numer(1, 1) = 22
-        numer(1, 2) = 39
-
-        Console.WriteLine(numer(0, 2))
-
-        Dim num As Integer = numer(1, 1)
-
-        Console.WriteLine(num)
-
-        For i = 0 To 1 Step 1
-
-            For j = 0 To 2 Step 1
-
-                Console.WriteLine(numer(i, j))
-
-            Next
-
-        Next
-
-        For Each nu As Integer In numer
-
-            Console.WriteLine(nu)
-
-        Next
-
-        For i = 0 To numer.GetUpperBound(0) - 1 Step 1
-
-            For j = 0 To numer.GetUpperBound(1) - 1 Step 1
-
-                Console.WriteLine(numer(i, j))
-
-            Next
+            Console.Write(num & " ")
 
         Next
 
