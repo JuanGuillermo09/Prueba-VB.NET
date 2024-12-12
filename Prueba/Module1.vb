f6d2efd6febb1afa6eaa5abc56ef6095112fb230
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-12 13:32:35 -0500

    Video 18 Ordenamiento por el método del burbuja

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -3,17 +3,15 @@
     Sub Main()
 
         Dim numeros = New Integer() {233, 301, 192, 169, 215}
+        For i = 0 To numeros.Length - 1 Step 1
 
-        For i = 1 To numeros.Length - 1 Step 1
+            For j = 0 To numeros.Length - 2 Step 1
 
-
-            For j = i To 1 Step -1
-
-                If numeros(j) < numeros(j - 1) Then
+                If numeros(j) > numeros(j + 1) Then
 
                     Dim aux As Integer = numeros(j)
-                    numeros(j) = numeros(j - 1)
-                    numeros(j - 1) = aux
+                    numeros(j) = numeros(j + 1)
+                    numeros(j + 1) = aux
 
                 End If
 
