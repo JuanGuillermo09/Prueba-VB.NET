3e75370f97fe3daf838d1f35f1c2f79710a60f85
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-12 15:13:45 -0500

    Video 19 Copiar contenido entre Arrays y Matrices

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -2,28 +2,73 @@
 
     Sub Main()
 
-        Dim numeros = New Integer() {233, 301, 192, 169, 215}
-        For i = 0 To numeros.Length - 1 Step 1
+        Dim numeros = New Integer() {14, 28, 56}
 
-            For j = 0 To numeros.Length - 2 Step 1
+        Dim arrayCopia(3) As Integer
 
-                If numeros(j) > numeros(j + 1) Then
+        For i = 0 To 2 Step 1
 
-                    Dim aux As Integer = numeros(j)
-                    numeros(j) = numeros(j + 1)
-                    numeros(j + 1) = aux
+            arrayCopia(i) = numeros(i)
 
-                End If
+        Next
+
+        For i = 0 To 2 Step 1
+
+            Console.Write(arrayCopia(i) & " ")
+
+        Next
+
+
+
+        Dim numero = New Integer() {14, 28, 56}
+
+        Dim arrayCopi(2) As Integer
+
+        For i = 0 To 2 Step 1
+
+            arrayCopi(i) = numero(i)
+
+        Next
+
+        For i = 0 To arrayCopi.Length - 1 Step 1
+
+            Console.Write(arrayCopi(i) & " ")
+
+        Next
+
+
+        Dim matrizOrigen = New Integer(,) {{14, 28}, {56, 70}}
+
+        Dim matrizCopia(2, 2) As Integer
+
+        For i = 0 To 1 Step 1
+
+            For j = 0 To 1 Step 1
+
+                matrizCopia(i, j) = matrizOrigen(i, j)
 
             Next
 
         Next
 
-        For Each num As Integer In numeros
+        For i = 0 To 1 Step 1
 
-            Console.Write(num & " ")
+            For j = 0 To 1 Step 1
+
+                Console.Write(matrizCopia(i, j) & " ")
+
+            Next
 
         Next
+
+
+
+
+
+
+
+
+
 
     End Sub
 
