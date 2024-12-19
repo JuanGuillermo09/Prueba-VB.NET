d6184bf60e1a5147a6150441f88d8988eb9079c4
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-19 15:02:12 -0500

    Mensaje descriptivo de lo que cambiaste

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -2,53 +2,19 @@
 
     Sub Main()
 
-        Dim numeros = New Integer() {1, 2, 3, 4}
+        Console.WriteLine("Mensaje del procedimiento main")
 
-        For Each num As Integer In numeros
-
-            Console.Write(num & "")
-
-        Next
-
-        ReDim Preserve numeros(5)
-
-        numeros(4) = 5
-        numeros(5) = 6
-
-        Console.WriteLine(" ")
-
-        For Each num As Integer In numeros
-
-            Console.Write(num & "")
-
-        Next
+        mostrarMansaje()
+        mostrarMansaje()
+        mostrarMansaje()
 
 
-
-        Dim numero = New Integer(,) {{1, 2}, {3, 4}}
-
-        ReDim numero(3, 3)
-
-        numero(0, 0) = 11
-        numero(0, 1) = 22
-        numero(0, 2) = 33
-        numero(1, 0) = 44
-        numero(1, 1) = 55
-        numero(1, 2) = 66
-        numero(2, 0) = 77
-        numero(2, 1) = 88
-        numero(2, 2) = 99
-
-        For i = 0 To 2 Step 1
-
-            For j = 0 To 2 Step 1
-
-                Console.Write(numero(i, j) & " ")
-            Next
-
-        Next
+    End Sub
 
 
+    Sub mostrarMansaje()
+
+        Console.WriteLine("Estamos dentro del procedimiento Sub")
 
     End Sub
 
