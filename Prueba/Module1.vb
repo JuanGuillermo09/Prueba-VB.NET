6a6da108ce8dae4695dae1cd6154949a08f9caa3
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-19 15:14:49 -0500

    Video 23 Parametros y Argumentos

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -1,20 +1,21 @@
 ﻿Module Module1
 
+    'Crear un procedimiento sub que permita mostrar un saludo a una persona, el nombre de la persona debe ser'
+    'pasado por parametro ' 
+
     Sub Main()
 
         Console.WriteLine("Mensaje del procedimiento main")
 
-        mostrarMansaje()
-        mostrarMansaje()
-        mostrarMansaje()
-
+        saludo("Maria")
+        saludo("Pedro")
 
     End Sub
 
 
-    Sub mostrarMansaje()
+    Sub saludo(nom As String)
 
-        Console.WriteLine("Estamos dentro del procedimiento Sub")
+        Console.WriteLine("Hola " & nom & " bienvenido(a) al mejor curso de Visual Basic. NET")
 
     End Sub
 
