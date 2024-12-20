add82d33071993b3dc29933312110695e93e8db2
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-20 11:55:56 -0500

    Video 26 Funciones predeterminadas para String

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -1,57 +1,19 @@
 ﻿Module Module1
 
-
     Sub Main()
 
-        Dim resultado As Integer = Sumar(20, 15)
+        Dim cadena1 As String = "Estamos en el mejor curso de Visual Basic. Net"
 
-        Console.WriteLine(Sumar(9, 8))
+        Dim cadena2 As String = "Estamos en el mejor Curso de Visual basic. Net"
 
-        Dim numeros = New Integer() {25, 48, 17, 33, 54, 99, 103}
+        Dim cadena3 As String = "Estamos en el mejor Curso"
 
-        Console.WriteLine(buscarNumeroArray(33, numeros))
-
+        Console.WriteLine(cadena1.ToUpper())
+        Console.WriteLine(cadena1.ToLower())
+        Console.WriteLine(cadena1.Substring(0, cadena3.Length()))
+        Console.WriteLine(cadena3.Length())
+        Console.WriteLine(cadena1.Equals(cadena2))
 
     End Sub
-
-    'Realiza un procedimiento que permita retornar la suma entre dos numeros enteros
-
-    Function Sumar(num1 As Integer, num2 As Integer) As Integer
-
-        Dim resultado As Integer = num1 + num2
-
-        Return resultado
-
-    End Function
-
-    'Realiza un procedimiento que permita buscar un numero en concreto dentro de un array,
-    ' retornando la posicion donde encontro el elemento buscado. 
-
-    Function buscarNumeroArray(numBuscar As Integer, vector() As Integer) As Integer
-
-        Dim pos As Integer = -1
-
-        Dim encontrado As Boolean = False
-
-        For i = 0 To vector.Length - 1 And encontrado = False Step 1
-
-            If vector(i) = numBuscar Then
-
-                pos = i
-
-                encontrado = True
-
-            End If
-
-        Next
-
-        Return pos
-
-    End Function
-
-
-
-
-
 
 End Module
