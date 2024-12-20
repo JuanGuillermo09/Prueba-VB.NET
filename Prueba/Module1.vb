4bea360a0949fb026c1cf5efe4c68f4267281a28
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-20 11:37:06 -0500

    Video 25 Procedimientos Function

--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -3,19 +3,55 @@
 
     Sub Main()
 
-        Dim numero As Integer = 5
+        Dim resultado As Integer = Sumar(20, 15)
 
-        cambiarNumeroVariable(numero, 45)
+        Console.WriteLine(Sumar(9, 8))
 
-        Console.WriteLine(numero)
+        Dim numeros = New Integer() {25, 48, 17, 33, 54, 99, 103}
 
-    End Sub
+        Console.WriteLine(buscarNumeroArray(33, numeros))
 
-
-    Sub cambiarNumeroVariable(ByRef num As Integer, numNuevo As Integer)
-
-        num = numNuevo
 
     End Sub
+
+    'Realiza un procedimiento que permita retornar la suma entre dos numeros enteros
+
+    Function Sumar(num1 As Integer, num2 As Integer) As Integer
+
+        Dim resultado As Integer = num1 + num2
+
+        Return resultado
+
+    End Function
+
+    'Realiza un procedimiento que permita buscar un numero en concreto dentro de un array,
+    ' retornando la posicion donde encontro el elemento buscado. 
+
+    Function buscarNumeroArray(numBuscar As Integer, vector() As Integer) As Integer
+
+        Dim pos As Integer = -1
+
+        Dim encontrado As Boolean = False
+
+        For i = 0 To vector.Length - 1 And encontrado = False Step 1
+
+            If vector(i) = numBuscar Then
+
+                pos = i
+
+                encontrado = True
+
+            End If
+
+        Next
+
+        Return pos
+
+    End Function
+
+
+
+
+
 
 End Module
