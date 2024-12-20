9b540ac40c0b1f560e86374ca078329c17a95195
Juan Guillermo Cardenas Miranda <juangui.1032@gmail.com>
2024-12-20 12:08:37 -0500

    Video 27 POO-Clases

---
+++ Prueba/AireAcondicionado.vb
@@ -0,0 +1,31 @@
+﻿Public Class AireAcondicionado
+
+    Public marca As String
+    Public modelo As String
+    Public color As String
+    Public voltaje As Integer
+    Public btu As Integer
+    Public temperatura As Integer
+
+    Public Sub subirTemperatuora()
+
+        temperatura += 1
+
+    End Sub
+
+    Public Sub bajasTemperatura()
+
+        temperatura -= 1
+
+    End Sub
+
+    Public Function darTemperatura() As Integer
+
+        Return temperatura
+
+    End Function
+
+
+
+
+End Class
--- Prueba/Module1.vb
+++ Prueba/Module1.vb
@@ -2,17 +2,7 @@
 
     Sub Main()
 
-        Dim cadena1 As String = "Estamos en el mejor curso de Visual Basic. Net"
 
-        Dim cadena2 As String = "Estamos en el mejor Curso de Visual basic. Net"
-
-        Dim cadena3 As String = "Estamos en el mejor Curso"
-
-        Console.WriteLine(cadena1.ToUpper())
-        Console.WriteLine(cadena1.ToLower())
-        Console.WriteLine(cadena1.Substring(0, cadena3.Length()))
-        Console.WriteLine(cadena3.Length())
-        Console.WriteLine(cadena1.Equals(cadena2))
 
     End Sub
 
